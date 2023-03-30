366d6bb9fffbc89a629b99387efe81c3028ced76
Anirudh Mishra <anirudh5mar@gmail.com>
2023-03-31 00:18:42 +0530

    Changed addr

--- redis/redis.go
+++ redis/redis.go
@@ -8,7 +8,7 @@ import (
 
 func RedisConnect() *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "127.0.0.1:6379",
 		Password: "",
 		DB:       0,
 	})
