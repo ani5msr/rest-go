e79e68cdba2c5ca23a91904ba12123c1f65f0ee6
Anirudh Mishra <anirudh5mar@gmail.com>
2023-03-31 14:21:14 +0530

    Created vendor

--- queue/interfaces.go
+++ queue/interfaces.go
@@ -1,11 +1,7 @@
 package queue
 
-import (
-	"github.com/gammazero/deque"
-)
-
 type QueueRest interface {
-	PushIn(string name, string val) (string response, error)
-	Pop(string name) (string response, error)
-	BqPOP(string name, string time) (string response, error)
+	PushIn(name string, val string) (response string, err error)
+	Pop(name string)
+	BqPOP(name string, time string) (response string, err error)
 }
