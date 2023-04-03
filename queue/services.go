a0a8e4342059301a53d89fcf58b4d260b5a3477b
Anirudh Mishra <anirudh5mar@gmail.com>
2023-04-03 20:32:08 +0530

    Created queue push func and endpoint

--- queue/services.go
+++ queue/services.go
@@ -1,1 +1,21 @@
 package queue
+
+import (
+	"sync"
+)
+
+type QueueInstance struct {
+	Q    []string
+	Lock sync.RWMutex
+	Name string
+}
+
+var qumap = make(map[string][]string)
+
+func (m *QueueInstance) PushIn(request QPushRequest) {
+	m.Q.PushBack(request.val)
+
+}
+func NewQueue(queue QueueRest, name string) {
+
+}
